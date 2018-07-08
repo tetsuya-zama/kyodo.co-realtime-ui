0d65a75412385130979b16f96f39ec09f5d743ae
tetsuya-zama <tetsuya.zama@gmail.com>
2018-07-08 02:24:19 +0000

    location関連本実装

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   GOOGLEMAP_API_KEY: JSON.stringify(process.env.GOOGLEMAP_API_KEY),
-  ENDPOINT_BASE_URL: JSON.stringify("https://api.rt.kyo-do.co")
+  ENDPOINT_BASE_URL: JSON.stringify("https://api.rt.kyo-do.co"),
+  POLLING_DURATION_MS: JSON.stringify(5000)
 }
--- src/apis/server/position.js
+++ src/apis/server/position.js
@@ -1,11 +1,13 @@
 import USER from '@/events/user'
+import USERLOCATION from '@/events/userlocation'
 import MEMBERLOCATIONS from '@/events/memberlocations'
+import axios from 'axios'
 
 let _instance = null
 
 export function loadPositionAPI (bus) {
   bus.$on(USER.LOGIN, logonUser => {
-    _instance = new PositionAPI(bus, logonUser.token)
+    _instance = new PositionAPI(bus, logonUser)
     _instance.startPolling()
     _instance.updatePositions()
   })
@@ -22,12 +24,18 @@ export function loadPositionAPI (bus) {
       _instance.updatePositions()
     }
   })
+
+  bus.$on(USERLOCATION.CHANGED, userLocation => {
+    if (_instance) {
+      _instance.sendUserLocation(userLocation)
+    }
+  })
 }
 
 class PositionAPI {
-  constructor (bus, token) {
+  constructor (bus, logonUser) {
     this.bus = bus
-    this.token = token
+    this.logonUser = logonUser
     this.timerId = null
   }
 
@@ -36,7 +44,7 @@ class PositionAPI {
       () => {
         this.updatePositions()
       },
-      5000 // XXX 設定ファイル等から取得？
+      process.env.POLLING_DURATION_MS
     )
   }
 
@@ -47,12 +55,43 @@ class PositionAPI {
   }
 
   updatePositions () {
-    // Dummy Logic
-    const dummyResult = [
-      {userid: 'userid123', position: { lat: 35.453793, lng: 139.597839 }, lastUpdate: '2017-03-31 15:30:31', name: '田中'},
-      {userid: 'userid345', position: { lat: 35.676975, lng: 139.773328 }, lastUpdate: '2018-01-31 15:30:31', name: '鈴木'}
-    ]
+    axios.get(
+      process.env.ENDPOINT_BASE_URL + '/position/all',
+      {
+        headers: {
+          Autorization: 'Bearer ' + this.logonUser.token
+        }
+      }
+    ).then(response => {
+      if (response.status === 200) {
+        this.bus.$emit(MEMBERLOCATIONS.UPDATED, response.data)
+      }
+    }).catch(() => {
+      // Dummy Logic
+      // TODO: API側が実装されたら消す
+      const dummyResult = [
+        {userid: 'userid123', position: { lat: 35.453793, lng: 139.597839 }, lastUpdate: '2017-03-31 15:30:31', name: '田中'},
+        {userid: 'userid345', position: { lat: 35.676975, lng: 139.773328 }, lastUpdate: '2018-01-31 15:30:31', name: '鈴木'}
+      ]
 
-    this.bus.$emit(MEMBERLOCATIONS.UPDATED, dummyResult)
+      this.bus.$emit(MEMBERLOCATIONS.UPDATED, dummyResult)
+    })
+  }
+
+  sendUserLocation (userLocation) {
+    axios.post(
+      process.env.ENDPOINT_BASE_URL + '/user/' + this.logonUser.userId + '/position',
+      {
+        position: {
+          lat: userLocation.coords.latitude,
+          lng: userLocation.coords.longitude
+        }
+      },
+      {
+        headers: {
+          Authorization: 'Bearer ' + this.logonUser.token
+        }
+      }
+    )
   }
 }
