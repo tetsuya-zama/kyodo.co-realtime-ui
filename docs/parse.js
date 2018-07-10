52229a344965d990cef26b009fe49c43e861ef37
tetsuya-zama <tetsuya.zama@gmail.com>
2018-07-10 09:42:43 +0000

    docs/ディレクトリ直下のすべて*.puファイルをpngに変換するように修正

--- docs/parse.js
+++ docs/parse.js
@@ -1,5 +1,19 @@
+/**
+ * docs以下にある*.puファイルを${元のファイル名}.pngにパースする
+ */
+
 var plantuml = require('node-plantuml');
 var fs = require('fs');
 
-var gen = plantuml.generate(__dirname + "/components.pu",{ format: 'png' });
-gen.out.pipe(fs.createWriteStream(__dirname + "/components.png"));
+fs.readdir(__dirname, (err, files) =>{
+    if (err) {
+        throw err;
+    }
+    
+    files
+        .filter(f => /^.*\.pu$/.test(f))
+        .forEach(f => {
+            const gen = plantuml.generate(__dirname + '/' + f,{ format: 'png' });
+            gen.out.pipe(fs.createWriteStream(__dirname + '/' + f + '.png'));
+        });
+});
