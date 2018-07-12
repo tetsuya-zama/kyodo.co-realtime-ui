ef3de0a0770c7c0e9f2bc5e5f2671dfa879aa3e1
EC2 Default User <ec2-user@ip-172-31-39-199.ap-southeast-1.compute.internal>
2018-07-12 14:35:20 +0000

    参加者の住所を表示

---
+++ src/components/home/MemberList.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-data-table
+    :items="allLocations"
+    hide-actions
+    class="elevation-1"
+  >
+    <template slot="items" slot-scope="row">
+      <td>{{ row.item.name }}</td>
+      <td>{{ row.item.position.lat }}</td>
+      <td>{{ row.item.position.lng }}</td>
+      <td>{{ row.item.address }}</td>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+import MEMBERLOCATIONS from '@/events/memberlocations'
+import {gmapApi} from 'vue2-google-maps'
+
+/**
+ * MemberLocation Component
+ * みんなの位置パネル
+ * @see https://github.com/Akryum/vue-googlemaps
+ * @prop {VueInstance} bus Event Bus
+ * @prop {Object} logonUser ログイン中のユーザー情報
+ * @prop {Object} userLocation ログオンユーザーの位置情報
+ * @state {Array} locations マップ上に表示すべき位置情報
+ */
+export default {
+  name: 'MemberList',
+  props: ['bus', 'userLocation'],
+  data: function () {
+    return {
+      locations: []
+    }
+  },
+  created: function () {
+    // みんなの位置情報更新イベントのハンドリング
+    this.bus.$on(MEMBERLOCATIONS.UPDATED, locations => {
+      this.locations = locations
+      return true
+    })
+  },
+  methods: {
+
+  },
+  computed: {
+    /**
+     * 表示すべき位置情報の前処理
+     */
+    allLocations: function () {
+      return this.locations
+    }
+  }
+}
+</script>
